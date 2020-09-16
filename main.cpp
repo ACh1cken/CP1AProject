b19c39af6417f300db77633ddebc45661f5298ea
lim20012233 <71307837+lim20012233@users.noreply.github.com>
2020-09-16 10:52:54 +0800

    Update main.cpp
    modify

--- main.cpp
+++ main.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 void Logo(){
     cout << R"(
-__  _                                                                                                       __  _ 
-\ `/ |                                                                                                      \ `/ |   
+__  _                                                                                                       __  _
+\ `/ |                                                                                                      \ `/ |
  \__`!                                                                                                       \__`!
- / ,' `-.__________________                  -------------------------------------------------------         / ,' `-.__________________  
-'-'\_____                LI`-.                Welcome to the Airline Transportation Booking System          '-'\_____                LI`-. 
-   <____()-=O=O=O=O=O=[]====--)              -------------------------------------------------------           <____()-=O=O=O=O=O=[]====--)   
-     `.___ ,-----,_______...-'                                                                                   `.___ ,-----,_______...-'  
-          /    .'                                                                                                     /    .'    
-         /   .'                                                                                                      /   .'         
-        /  .'                                                                                                       /  .'         
+ / ,' `-.__________________                  -------------------------------------------------------         / ,' `-.__________________
+'-'\_____                LI`-.                Welcome to the Airline Transportation Booking System          '-'\_____                LI`-.
+   <____()-=O=O=O=O=O=[]====--)              -------------------------------------------------------           <____()-=O=O=O=O=O=[]====--)
+     `.___ ,-----,_______...-'                                                                                   `.___ ,-----,_______...-'
+          /    .'                                                                                                     /    .'
+         /   .'                                                                                                      /   .'
+        /  .'                                                                                                       /  .'
         `-'                                                                                                         `-'
 
 
@@ -28,6 +28,71 @@ void MainMenu(){
     }while(choice == 'Y' | 'y' );
 }
 
+void modify(Passanger &i){
+int choice;
+string newname;
+int newarrivaltime;
+string newdestination;
+int newdepaturetime;
+
+cout<< " what do you want to modify ?"<<endl<<endl;
+
+cout<< "1) modify your name"<<endl;
+cout<< "2) modify your destination"<<endl;
+cout<< "3) modify your arrival time"<<endl;
+cout<< "4) modify your depature time"<<endl;
+
+cout<< " Enter the number you want to modify."<<endl;
+cin>>choice;
+
+do{
+        switch(choice)
+        {
+        case 1 :
+                cout<<"Enter your new name"<<end;
+                cin>>newname;
+                i.Name = newname;
+                cout<<"new name updated !"<<endl;;
+
+                break;
+        case 2 :
+                cout<<"Enter your new destination"<<end;
+                cin>>newdestination;
+                i.Destination = newdestination;
+
+                cout<<"new destination updated"<<endl;
+                break;
+        case 3 :
+                cout<<"Enter your new arrival time"<<endl;
+                cin>>newarrivaltime;
+                i.ArrivalTime = newarrivaltime;
+                cout<<"new arrival time !"<<endl;;
+
+                break;
+
+        case 4 :
+                cout<<"Enter your new departure time"<<endl;
+                cin>>newdepaturetime;
+                i.DepatureTime = newdepaturetime;
+
+                cout<<"new name departure time updated !"<<endl;;
+
+
+                break;
+
+        }
+
+
+
+
+}while(choice<4 && choice>=1);
+
+
+
+}
+
+
+
 
 int main(){
 
@@ -38,3 +103,5 @@ int main(){
 
     return 0;
 }
+
+
