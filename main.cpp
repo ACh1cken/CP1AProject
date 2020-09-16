4b2adef74486ff91822e236d222b1654ead4f501
ACh1cken <68433261+ACh1cken@users.noreply.github.com>
2020-09-16 09:09:16 +0800

    Added logo

--- main.cpp
+++ main.cpp
@@ -2,6 +2,39 @@
 #include<fstream>
 using namespace std;
 
+void Logo(){
+    cout << R"(
+__  _                                                                                                       __  _ 
+\ `/ |                                                                                                      \ `/ |   
+ \__`!                                                                                                       \__`!
+ / ,' `-.__________________                  -------------------------------------------------------         / ,' `-.__________________  
+'-'\_____                LI`-.                Welcome to the Airline Transportation Booking System          '-'\_____                LI`-. 
+   <____()-=O=O=O=O=O=[]====--)              -------------------------------------------------------           <____()-=O=O=O=O=O=[]====--)   
+     `.___ ,-----,_______...-'                                                                                   `.___ ,-----,_______...-'  
+          /    .'                                                                                                     /    .'    
+         /   .'                                                                                                      /   .'         
+        /  .'                                                                                                       /  .'         
+        `-'                                                                                                         `-'
+
+
+    )"<<endl;
+}
+
+void MainMenu(){
+    char choice;
+    do {
+        cout << "Do you want to exit the program?";
+        cin >> choice;
+    }while(choice == 'Y' | 'y' );
+}
+
+
 int main(){
+
+
+    Logo();
+    cout <<"Test";
+    MainMenu();
+
     return 0;
 }
